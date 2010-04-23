8e621948f44c7782057c0e8d5928552ee2fb13e1
rbulha@gmail.com <rbulha@gmail.com@d4794bea-eb5e-33b8-b45c-84b48bc2d2c9>
2010-04-23 02:40:30 +0000

    -Receiving events from TCP server. -full integrated with SMplayer.

--- trunk/RemoteWifi_PC.py
+++ trunk/RemoteWifi_PC.py
@@ -1,7 +1,29 @@
 import sys
 import wx
+import wx.lib.newevent
+import SocketServer
+import os
 
-from GUI_xrc import xrcwxMainFrame
+from GUI_xrc   import xrcwxMainFrame
+from Server_PC import CServer
+
+import SMPlayer_Wrapper as TARGET_1
+
+(ReceivedDataEvent, EVT_RECEIVED_DATA) = wx.lib.newevent.NewEvent()
+
+class CTCPHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "%s wrote:" % self.client_address[0]
+        print self.data
+        try:
+            MainFrame = wx.GetApp().GetTopWindow() 
+            if MainFrame:
+                evt = ReceivedDataEvent(data=self.data)
+                wx.PostEvent(MainFrame,evt)
+        except:
+            print "CTCPHandler - fail to get the TopWindow"    
 
 class CMainFrame(xrcwxMainFrame):
     def __init__(self, parent):
@@ -9,7 +31,33 @@ class CMainFrame(xrcwxMainFrame):
         iconFile = "res/Probe.ico"
         icon1 = wx.Icon(iconFile, wx.BITMAP_TYPE_ICO)
         self.SetIcon(icon1)
-        
+        self.Bind(EVT_RECEIVED_DATA,self.OnDataReceived)
+    def OnDataReceived(self,evt):
+        print evt.data  
+    def OnButton_wxServerStartButton(self, evt):
+        ip = self.wxIPTextctrl.GetValue()
+        port = int(self.wxPortTextctrl.GetValue())
+        print '[CMainFrame:OnButton_wxServerStartButton] - Start server: (%s,%d)'%(ip,port)
+        server = CServer(ip, port, CTCPHandler)
+    def OnButton_wxOpenTargetButton(self, evt):
+        TARGET_1.TARGET_PROCESS_PATH = os.path.dirname(self.wxSelectTargetCombo.GetValue())  
+        TARGET_1.TARGET_PROCESS_NAME = os.path.split(self.wxSelectTargetCombo.GetValue())[1]
+        media_file = self.wxMediaFileCombo.GetValue()
+        print 'Open SMPlayer (%s,%s,%s)'%(TARGET_1.TARGET_PROCESS_NAME,TARGET_1.TARGET_PROCESS_PATH,media_file)
+        TARGET_1.SendTarget('open',media_file)
+    def OnButton_wxPlayTargetButton(self, evt):
+        TARGET_1.TARGET_PROCESS_PATH = os.path.dirname(self.wxSelectTargetCombo.GetValue())  
+        TARGET_1.TARGET_PROCESS_NAME = os.path.split(self.wxSelectTargetCombo.GetValue())[1]
+        TARGET_1.SendTarget('play')
+    def OnButton_wxPauseButton(self, evt):
+        TARGET_1.TARGET_PROCESS_PATH = os.path.dirname(self.wxSelectTargetCombo.GetValue())  
+        TARGET_1.TARGET_PROCESS_NAME = os.path.split(self.wxSelectTargetCombo.GetValue())[1]
+        TARGET_1.SendTarget('pause')
+    def OnButton_wxFullscreenTargetButton(self, evt):
+        TARGET_1.TARGET_PROCESS_PATH = os.path.dirname(self.wxSelectTargetCombo.GetValue())  
+        TARGET_1.TARGET_PROCESS_NAME = os.path.split(self.wxSelectTargetCombo.GetValue())[1]
+        TARGET_1.SendTarget('fullscreen')
+                              
 class CRemoteApp(wx.App):
   def __init__(self, filename="Remote.ini"):
     wx.App.__init__(self,0)
@@ -18,6 +66,7 @@ class CRemoteApp(wx.App):
     #self.MainFrame.Bind(wx.EVT_MENU, self.OnUserClose, id=self.SimulationFrame.menuBar.menuID1)
     self.MainFrame.Show() 
     #self.MainFrame.Iconize(True)
+    #print 'Teste: ',wx.GetApp()
 
 def main():
     if len(sys.argv) == 2 and sys.argv[1] == 'reserved1':
